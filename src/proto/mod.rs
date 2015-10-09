d2839797906cdeeab4410ef8e25dc0b71795bbe2
Hyunsik Choi <hyunsik@apache.org>
2015-10-09 13:07:36 -0700

    Add proto module and rust-protobuf dependency.

---
+++ src/proto/mod.rs
@@ -0,0 +1,12 @@
+pub mod acl;
+pub mod ClientDatanodeProtocol;
+pub mod ClientNamenodeProtocol;
+pub mod datatransfer;
+pub mod encryption;
+pub mod hdfs;
+pub mod inotify;
+pub mod IpcConnectionContext;
+pub mod ProtobufRpcEngine;
+pub mod RpcHeader;
+pub mod Security;
+pub mod xattr;
